fd43b2f52c1eabcc3fbaca8661976807ad8f5cdc
Steve Brownlee <chortlehoort@gmail.com>
2021-04-02 17:20:18 -0500

    Include images

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 rm -rf dist
 mkdir -p dist/styles
 mkdir -p dist/scripts
+mkdir -p dist/images
 cp src/index.html dist
 cp src/styles/* dist/styles
+cp src/images/* dist/images
 javascript-obfuscator ./src/scripts --output dist/scripts
